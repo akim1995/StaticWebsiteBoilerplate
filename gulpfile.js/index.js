b6351ba7f98ef5dd046c4ab27c13e882d59afb11
akim1995 <a.khalitov@clientlab.ru>
2018-12-28 16:15:43 +0200

    added compressed css output

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -7,7 +7,7 @@ sass.compiler = require('node-sass');
 function sassTask() {
 	return src('assets/scss/**/*.scss')
 		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(sourcemaps.write())
 		.pipe(dest('css/'));
 }
